ce21199b4a1d3b4ee12fc813bb4a0aefc99828b2
nichlor <70553339+nichlor@users.noreply.github.com>
2021-06-28 08:37:32 -0400

    Update keygen.sh
    - Removed "." from .receiving-addresses.txt (this causes the file to be hidden by default in terminal)
    removed n variable 
    set default to generates 50 wallets

--- keygen.sh
+++ keygen.sh
@@ -2,11 +2,10 @@
 
 ## NOTES ##
 
-# This script will add N substrate wallet private keys in raw format to keys.txt 
+# This script will add 50 substrate wallet private keys in raw format to keys.txt 
  
-N=50
 
-for A in {1..11}; do subkey generate -n kusama >> /tmp/keys.txt; done
+for A in {1..50}; do subkey generate -n kusama >> /tmp/keys.txt; done
 cat /tmp/keys.txt | grep "Secret seed:"|sed 's/Secret seed://g'|tr -d ' ' >> keys.txt \
-&& cat /tmp/keys.txt | grep "SS58 Address:"|sed 's/SS58 Address://g'|tr -d ' ' >> .receiving-addresses.txt \
+&& cat /tmp/keys.txt | grep "SS58 Address:"|sed 's/SS58 Address://g'|tr -d ' ' >> receiving-addresses.txt \
 && rm /tmp/keys.txt
